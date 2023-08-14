fc3f238bb664e1f8ab90999bb4f40e96e98a780b
SilasMoreti <127680029+SilasMoreti@users.noreply.github.com>
2023-08-14 19:33:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function limpar() {
+    document.getElementById('ntf-qtd').innerHTML = 0;
+    document.getElementById('ntf-cln1').style.background = 'none'
+    document.getElementById('ntf-cln2').style.background = 'none'
+    document.getElementById('ntf-cln3').style.background = 'none'
+    document.querySelector("div.nao-lido").style.background = 'none'
+    document.querySelector("p.nao-lido").style.background = 'none'
+    document.querySelector("span.nao-lido").style.background = 'none'
+} 
