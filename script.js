c9f3ecfd0e1c33778398f48f75e4af66f6aaf5c1
SilasMoreti <127680029+SilasMoreti@users.noreply.github.com>
2023-08-14 23:59:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function limpar() {
     document.getElementById('ntf-cln1').style.background = 'none'
     document.getElementById('ntf-cln2').style.background = 'none'
     document.getElementById('ntf-cln3').style.background = 'none'
-    document.querySelector("div.nao-lido").style.background = 'none'
-    document.querySelector("p.nao-lido").style.background = 'none'
-    document.querySelector("span.nao-lido").style.background = 'none'
+    document.getElementById('nao-lido').style.display = 'none'
+    document.querySelector('span.nao-lido').style.display = 'none'
+    document.querySelector('span#nao-lidoo').style.display = 'none'
 } 
